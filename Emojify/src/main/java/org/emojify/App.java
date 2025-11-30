4d9c2ab33de5c4cb687dd36394d793717ded62bd
sengA7 <agnes.dupont@etudiant.univ-lr.fr>
2025-11-30 17:59:06 +0900

    Setup maven, add main entry point, demo test, update README

---
+++ Emojify/src/main/java/org/emojify/App.java
@@ -0,0 +1,8 @@
+package org.emojify;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Emojify project initialized");
+    }
+}
---
+++ Emojify/src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void demoTest(){
+        assertEquals(2, 1+1);
+    }
+}
