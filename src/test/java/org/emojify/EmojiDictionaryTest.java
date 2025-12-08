ba2e431bb926089b2bfdd7bcace7c28e14467019
Minhee Jeong <jungminhee0730@naver.com>
2025-12-08 22:54:50 +0900

    imported assertEquals

--- src/test/java/org/emojify/EmojiDictionaryTest.java
+++ src/test/java/org/emojify/EmojiDictionaryTest.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class EmojiDictionaryTest {
