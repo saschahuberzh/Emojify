f92089b9a55e2862e6c8bb932f05e6a9437e1f61
na hyanghee <gidgml514863@gmail.com>
2025-12-10 22:56:00 +0900

    Fix compilation error in EmojiDictionaryTest

--- src/test/java/org/emojify/EmojiDictionaryTest.java
+++ src/test/java/org/emojify/EmojiDictionaryTest.java
@@ -4,7 +4,7 @@ import java.util.* ;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 
