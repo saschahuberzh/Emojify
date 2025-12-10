c9476cb4b07b2d10f12459947c1d07c91cbc0a05
Sascha Huber <74604940+saschahuberzh@users.noreply.github.com>
2025-12-10 19:05:56 +0900

    [#7] Issue 6,Basic terminal interaction added (#12)
    * Basic terminal interaction added
    
    * Output message changed
    
    * solved type

--- src/main/java/org/emojify/App.java
+++ src/main/java/org/emojify/App.java
@@ -1,8 +1,35 @@
 package org.emojify;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Emojify project initialized");
+        ConsoleInterface console = new ConsoleInterface();
+        boolean running = true;
+        //read word from json
+
+        while (running) {
+
+            int choice = console.showMenu("Menu", List.of(
+                    "Parse emoji",
+                    "Finish app"
+            ));
+
+            switch (choice) {
+
+                case 1 -> {
+                    String text = console.readLine("Add text to parse to emoji:");
+                    //call method to parse
+                    console.print(text);
+                }
+
+                case 2 -> {
+                    running = false;
+                    console.print("Closing app ...");
+                    console.close();
+                }
+            }
+        }
     }
 }
